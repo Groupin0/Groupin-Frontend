620d2902eec3acb1da99db4e192b87a665295d74
Annie <Anniebelkin97@gmail.com>
2020-04-01 11:54:59 +0300

    change InputField to Input. update SearchBar, Button and AddSession to new Input path. update addSession form

--- src/components/Shared/AddSession.js
+++ src/components/Shared/AddSession.js
@@ -1,35 +1,49 @@
 import React from 'react';
 import Button from '../Button/Button.js';
-import InputField from '../InputField/InputField.js';
+import Form from '../Form/Form.js';
+import Input from '../Form/Input.js';
+import Dropdown from '../Form/Dropdown.js';
+import Category from './Category.js';
 
 const AddSession = props => {
 
+    const onSubmit = data => {
+        console.log(data);
+    }
+
     return (
         <>
-        <div className='Modal__content--header'>
-            <h1 className=''>צור מפגש</h1>
-        </div>
-        <div className='Modal__content--body'>
-            <InputField 
-                label= 'שם המפגש'
-                name= 'sessionName' 
-                type= 'text'
-                value= 'sessionName'
-                placeholder= 'הכנס שם מפגש...'
-                className=' InputField__border'
-            />
-            <InputField 
-                label= 'קטגוריה'
-                name= 'category' 
-                type= 'text'
-                value= 'caterogy'
-                placeholder= 'בחר קטגוריה...'
-                className=' InputField__border'
-            />
-        </div>
-        <div className='Modal__content--actions'>
-            <Button label='צור מפגש' type='submit' className='Button__black' onClick={ props.onSubmit }/>
-        </div> 
+        <Form title={<p className='Form__title'>צור מפגש</p>} onSubmit={onSubmit}>
+                <Input 
+                    name= 'sessionName' 
+                    type= 'text'
+                    placeholder= 'הכנס שם מפגש...'
+                    className='Form__input'
+                    required
+                />
+                <Dropdown 
+                    name= 'sessionCategory' 
+                    options= {Category}
+                    className='Form__input'
+                    required
+                />
+                <Input 
+                    name= 'sessionDate' 
+                    type= 'date'
+                    placeholder= 'הכנס שם מפגש...'
+                    className='Form__input Form__input--2'
+                    required
+                />
+                <Input 
+                    name= 'sessionTime' 
+                    type= 'text'
+                    placeholder= ' hh:mm '
+                    className='Form__input Form__input--2'
+                    pattern="([01]?[0-9]|2[0-3]):[0-5][0-9]"
+                    required
+                />
+                <Button label='צור מפגש' type='submit' className='Button__black Form__button'/>
+        </Form>
         </>
     );
 }

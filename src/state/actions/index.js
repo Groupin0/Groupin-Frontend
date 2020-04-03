a274efdca3189bd9f862b89ef9989af73926bc05
ozizhakGT <ozizhak5540@gmail.com>
2020-04-04 01:51:27 +0300

    create authentication login/sign up with facebook

--- src/state/actions/index.js
+++ src/state/actions/index.js
@@ -65,7 +65,7 @@ export const getUser = () => async dispatch => {
 
         dispatch({type: userActions.FETCH_USER, payload: response.data.UserDetailed});
     } catch (e) {
-        console.dir(e);
+        console.error(e);
     }
 };
 

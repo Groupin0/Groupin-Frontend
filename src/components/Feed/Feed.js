4db5b319824e4e76b9a065af70a2f0c983d64c7a
ozizhakGT <ozizhak5540@gmail.com>
2020-04-04 21:20:07 +0300

    create info sessions

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 import './Feed.scss';
 import SessionCube from "../SessionCube/SessionCube";
 import {shallowEqual, useSelector} from "react-redux";
 import Spinner from "../Shared/Spinner/Spinner";
 
-const Feed = ({isUserSessions}) => {
+const Feed = ({isUserSessions, loadMoreSession}) => {
+    const [loadMore, setLoadMore] = useState(true);
+    const [loading, setLoading] = useState(false)
     const {categories, sessions} = useSelector(state => ({
         categories: state.categories,
         sessions: state.sessions
@@ -16,9 +18,23 @@ const Feed = ({isUserSessions}) => {
             data={{...session, category: categories.find(c => c.id === session.category)}}
             className={(i === 0 && !isUserSessions) ? 'SessionCube__first' : ''} key={i}/>
     });
+
+    const onLoadMoreSession = async () => {
+        setLoading(true);
+        const result = await loadMoreSession(sessions.length);
+
+        if (!result) {
+            setLoadMore(false)
+        }
+        setLoading(false)
+    };
+
+    console.log(loadMore);
     return (
         <div className='Feed'>
-            {(sessions && categories) ? renderSessions() : <Spinner />}
+            {(sessions && categories) ? renderSessions() : <Spinner/>}
+            {loadMore && <div className='Feed__load-more' onClick={onLoadMoreSession}>{!loading ? 'טען עוד מפגשים...' :
+                <Spinner/>}</div>}
         </div>
     )
 };
--- src/state/reducers/sessionsReducer.js
+++ src/state/reducers/sessionsReducer.js
@@ -4,6 +4,8 @@ export default (state=[], action) => {
     switch (action.type) {
         case sessionActions.FETCH_SESSIONS:
             return action.payload;
+        case sessionActions.LOAD_MORE_SESSIONS:
+            return [...state, ...action.payload];
         case sessionActions.CREATE_SESSION:
             return [action.payload, ...state];
         default:
